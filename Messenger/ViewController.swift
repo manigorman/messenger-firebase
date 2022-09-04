6281efd4f6a5b1719a55ddd2a8e60af1f35c8aa3
manigorman <immanakov@gmail.com>
2022-09-04 21:00:20 +0300

    Настроил проект

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Igor Manakov on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # messenger-firebase
+## Project launch
+Make sure that you have these versions before starting the project:
+
+> Ruby 2.7.5 (rbenv usage recommended)\
+> Bundler 2.3.21
+
+Installing Cocoapods dependencies 
+```
+bundle exec pod install
+```
